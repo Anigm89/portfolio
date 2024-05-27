86d30773c44f0cea28eaeeffc3474ded22f36f8f
Aniana <anigm89@gmail.com>
2024-05-27 19:19:43 +0200

    home terminada

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -17,22 +17,50 @@ function Home(){
                     En el frontend, utilizo React para construir interfaces de usuario dinámicas y eficientes, aprovechando sus ventajas para crear experiencias de usuario ricas y responsivas.</p>
                 <p>Me apasiona seguir aprendiendo y mejorando mis habilidades, adoptando nuevas tecnologías y mejores prácticas para ofrecer soluciones innovadoras y de alta calidad. Estoy siempre en busca de nuevos retos y oportunidades para crecer profesionalmente en el emocionante mundo del desarrollo web.</p>
             </div>
-            <div>
+            <div className='seccion'>
                 <h2>Mis proyectos</h2>
                 <ul className="listado-trabajos">
-                    <li className='visible proyecto1'>
+                    <li className='proyecto1'>
                         <div className='datacontainer'>
                             <h3>Dashboard</h3>
-                            <p className='description'>hola</p>
+                            <p className='description'>Proyecto creado con  HTML5, CSS y JavaScript Vanilla</p>
                             <Link to="/proyecto1" className='ver'>Ver</Link>
                         </div>
-
                     </li>
-                    <li className='visible proyecto1'>2</li>
-                    <li className='visible proyecto1'>3</li>
+                    <li className='proyecto2'>
+                        <div className='datacontainer'>
+                            <h3>Tienda</h3>
+                            <p className='description'>Proyecto de backend creado con NodeJS y ExpressJS, HTML5, CSS y MongoDB.<br/> Y Firebase auth para la autenticación.</p>
+                            <Link to="/proyecto2" className='ver'>Ver</Link>
+                        </div>
+                    </li>
+                    <li className='proyecto3'>
+                    <div className='datacontainer'>
+                            <h3>AppBook</h3>
+                            <p className='description'>App de libros creada con NodeJS, ExpressJS y MySQL para el backend, React para el front-end y Firebase auth para la autenticación.</p>
+                            <Link to="/proyecto3" className='ver'>Ver</Link>
+                        </div>
+                    </li>
                 </ul>
             </div>
+            <div className='seccion'>
+                <h2>Contacto</h2>
+                <div className='enlaces'>
+                    <a href="https://www.linkedin.com/in/aniana-gonzalez-moreno/" target='_blank' className='btn'>
+                      <div className='ln'></div>
+                    </a>
+                    <a href="mailto:anigm89@gmail.com" target='_blank'  className='btn'>
+                        <div className='mail'></div>
+                    </a>
+                </div>
+            </div>
         </section>
+        <footer>
+            <p>Creado y desarrollado por Ani González Moreno</p>
+            <a href="https://github.com/Anigm89/" target='_blank'>
+                <img src="../../assets/img/github.png" alt="github" />
+            </a>
+        </footer>
         </>
     )
 }
